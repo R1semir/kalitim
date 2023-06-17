8fb09220222716ba533bbc4e15b3da43b4fd92dc
Emir <emirkan.ulker9@gmail.com>
2023-06-18 00:00:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _kalıtımm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ucak uck = new ucak();
+            uck.AD = "emir";
+            uck.SOYAD = "ülker";
+            uck.YAS = -22;
+            uck.CINSIYET = "erkek";
+            uck.Marka = "türk hava yolları";
+            uck.KALKIS = "ısparta";
+            uck.VARIS = "adana";
+
+            Console.WriteLine("Yolcu Adı: " + uck.AD);
+            Console.WriteLine("Yolcu Soyadı: " + uck.SOYAD);
+            Console.WriteLine("Yolcu Yaşı: " + uck.YAS);
+            Console.WriteLine("Yolcu Cinsiyeti: " + uck.CINSIYET);
+            Console.WriteLine("Uçak Markası: " + uck.Marka);
+            Console.WriteLine("Uçak Kalkış Noktası: " + uck.KALKIS);
+            Console.WriteLine("Uçak Varış Noktası: " + uck.VARIS);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ucak.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _kalıtımm
+{
+    internal class ucak:yolcu
+    {
+        private string marka;
+        private string varıs;
+        private string kalkis;
+
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value.ToUpper(); }
+        }
+
+        public string VARIS
+        {
+            get { return varıs; }
+            set { varıs = value.ToLower(); }
+        }
+
+        public string KALKIS
+        {
+            get { return kalkis; }
+            set { kalkis = value.ToUpper(); }
+        }
+    }
+}
